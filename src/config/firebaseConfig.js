ccc517ddbaa46d938b2479472ca2c97b72aa6716
Vishal Das <vdas53073@gmail.com>
2023-04-05 17:04:17 +0530

    Firebase integration

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyALtJNhOnrzGD_pybaT0roU8bi7JvjTC8g",
+  authDomain: "crypto-hunter-46ccd.firebaseapp.com",
+  projectId: "crypto-hunter-46ccd",
+  storageBucket: "crypto-hunter-46ccd.appspot.com",
+  messagingSenderId: "120349901748",
+  appId: "1:120349901748:web:86eb78c6f52f8d072568e1"
+};
+
+export default firebaseConfig;
